37283f21f52d273d136422d6ed2d8b9e401b89fc
Kristjan Bajuk <kristjan.bajuk@gmail.com>
2025-12-19 15:46:43 +0100

    Revert "fix: aggregate pending revenues for the same user in the revenue distribution"
    This reverts commit 94cd4cd22d1bd32f6a63f70f9aadaf463dcbcc5d.

--- src/handler/PropertyRevenueDistribution.ts
+++ src/handler/PropertyRevenueDistribution.ts
@@ -27,18 +27,14 @@ PropertyRevenueDistribution.RevenueAdded.handler(async ({ event, context }) => {
   }, {} as { [key: string]: any });
 
 
-    // There can be multiple entries for the same user. We need to aggregate all the amounts for the same user.
-    const aggregatedPendingRevenues = event.params.users.reduce<Record<string, bigint>>((acc, user, index) => {
-        acc[user] = (acc[user] ?? 0n) + event.params.amounts[index];
-        return acc;
-    }, {});
-
-    for (const [user, totalPendingRevenue] of Object.entries(aggregatedPendingRevenues)) {
+    for  (const [index, user] of event.params.users.entries()) {
         const propertyTokenRevenue = propertyTokenRevenuesLoaded[user];
+
         context.PropertyTokenRevenue.set({
           ...propertyTokenRevenue,
-          pendingRevenue: propertyTokenRevenue.pendingRevenue + totalPendingRevenue,
-        });
+          pendingRevenue:
+              propertyTokenRevenue.pendingRevenue + event.params.amounts[index],
+          });
     }
 
       const currentPropertyTokenRevenueDistribution =
