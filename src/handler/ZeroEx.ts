81531e3393837ee41d1733caea0129e00bb98591
Kristjan Bajuk <kristjan.bajuk@gmail.com>
2026-01-14 20:08:48 +0100

    fix: add enums for handlers

--- src/handler/ZeroEx.ts
+++ src/handler/ZeroEx.ts
@@ -1,5 +1,6 @@
 import { ZeroEx } from 'generated';
 import { updatePropertyTokenTradeCounts } from '../helper/LimitOrderTrades';
+import {LimitOrderProtocol} from "../types/enums";
 
 ZeroEx.LimitOrderFilled.handler(async ({ event, context }) => {
   // Check if either makerToken or takerToken is a property token
@@ -28,7 +29,7 @@ ZeroEx.LimitOrderFilled.handler(async ({ event, context }) => {
     takerTokenFilledAmount: event.params.takerTokenFilledAmount,
     makerTokenFilledAmount: event.params.makerTokenFilledAmount,
     propertyValuation: propertyToken.propertyValuation,
-    protocol: 'ZeroEx',
+    protocol: LimitOrderProtocol.ZeroEx,
   });
 
   // Handle case where property token is on maker side (being sold)
